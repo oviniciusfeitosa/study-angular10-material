c9f2ded711ee529b2921b73e7be95af7447cf7d3
vinnyfs89 <viniciusfesil@gmail.com>
2020-05-17 16:22:02 -0300

    🐛 Removing trash

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
   ],
   imports: [
     BrowserModule,
