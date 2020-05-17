1870d8abbbbb212a90f3c8aea1b822a34b8a9f07
vinnyfs89 <viniciusfesil@gmail.com>
2020-05-17 18:56:45 -0300

    💄  Display details when click

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -13,10 +13,11 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   ngOnInit(): void {
   }
