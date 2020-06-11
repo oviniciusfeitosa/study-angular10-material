97fffcb6b08f0d26f46064ba689ed4410a4946c7
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-06-11 13:30:48 -0300

    :recycle: Arrange repos

--- ngrx/src/app/github/github.module.ts
+++ ngrx/src/app/github/github.module.ts
@@ -5,9 +5,18 @@ import { GithubRoutingModule } from "./github-routing.module";
 import { GithubComponent } from "./github.component";
 import { MatListModule } from "@angular/material/list";
 import { MatChipsModule } from "@angular/material/chips";
+import { MatCardModule } from "@angular/material/card";
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
   declarations: [GithubComponent],
-  imports: [CommonModule, GithubRoutingModule, MatListModule, MatChipsModule],
+  imports: [
+    CommonModule,
+    GithubRoutingModule,
+    MatListModule,
+    MatChipsModule,
+    MatCardModule,
+    MatTableModule,
+  ],
 })
 export class GithubModule {}
