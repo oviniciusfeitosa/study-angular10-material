abf311090b2c914b19d9b6b0aa6fb2ac38428506
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-06-11 17:43:54 -0300

    :recycle: Set routes by modules

--- ngrx/src/app/app-routing.module.ts
+++ ngrx/src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { AppComponent } from "./app.component";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./shared/home/home.component";
 
 const routes: Routes = [
   {
     path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "github",
     loadChildren: () =>
       import("./github/github.module").then((m) => m.GithubModule),
+  },
+  {
+    path: "ngrx-counter",
+    loadChildren: () =>
+      import("./ng-rx-counter/ng-rx-counter.module").then(
+        (m) => m.NgRxCounterModule
+      ),
   },
 ];
 
