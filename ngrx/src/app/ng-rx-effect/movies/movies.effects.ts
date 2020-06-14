1fd8017850e5e4b53ebb06348dad6ad3bfd61ef1
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-06-14 00:15:01 -0300

    :sparkles: create movie effect/

---
+++ ngrx/src/app/ng-rx-effect/movies/movies.effects.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { MoviesService } from "./movies.service";
+import { mergeMap, map, catchError } from "rxjs/operators";
+import { EMPTY } from "rxjs";
+
+@Injectable()
+export class MoviesEffects {
+  loadMovies$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType("[Movies Page] Load Movies"),
+      mergeMap(() =>
+        this.moviesService.getAll().pipe(
+          map((movies) => ({
+            type: "[Movies API] Movies Loaded Success",
+            payload: movies,
+          })),
+          catchError(() => EMPTY)
+        )
+      )
+    )
+  );
+
+  constructor(
+    private actions$: Actions,
+    private moviesService: MoviesService
+  ) {}
+}
